5afea90d7d66492dfdf374ea496d63861a9e62e8
Ga120865 <143370702+Ga120865@users.noreply.github.com>
2025-12-12 01:36:37 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-// firebase.js (coloca exactamente este archivo en la raíz)
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
 
