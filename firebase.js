f6fb41e32339b3cfcd349e43260427cd926bb262
Ga120865 <143370702+Ga120865@users.noreply.github.com>
2025-12-11 23:19:32 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-// firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
-export const firebaseConfig = {
-  apiKey: "AIzaSyA9kNtwjDWTooEHEBcLe03FXsXlxcyaGGM",
-  authDomain: "fresme-17315.firebaseapp.com",
-  databaseURL: "https://fresme-17315-default-rtdb.firebaseio.com",
-  projectId: "fresme-17315",
-  storageBucket: "fresme-17315.firebasestorage.app",
-  messagingSenderId: "313000627548",
-  appId: "1:313000627548:web:91478f1e3dd3c342c74599",
-  measurementId: "G-3B3QYZCLDS"
+const firebaseConfig = {
+  apiKey: "TU_APIKEY",
+  authDomain: "fresme-app.firebaseapp.com",
+  databaseURL: "https://fresme-app-default-rtdb.firebaseio.com",
+  projectId: "fresme-app",
+  storageBucket: "fresme-app.firebasestorage.app",
+  messagingSenderId: "68669835916",
+  appId: "1:68669835916:web:ed424d557a616fb37149a9",
+  measurementId: "G-RPSCTLQ9RG"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
